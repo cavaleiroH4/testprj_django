bc33a11d8dba962c06860a53185da7752aa055d7
João Eduardo Ferreira <joao.eduardoferco@gmail.com>
2022-04-27 18:51:31 -0300

    Definindo parciais de html css pra homepage

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'global/home.html')
+    return render(request, 'templates/recipes/pages/home.html')
 
 
 # Create your views here.
